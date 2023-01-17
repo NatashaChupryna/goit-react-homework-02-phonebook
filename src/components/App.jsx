a9eaff2fd3c854c7723fce2b606fd689fa0afeca
Natasha <natkamonte1992@gmail.com>
2023-01-17 13:39:38 +0200

    hw2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+import { nanoid } from 'nanoid';
+import { StyledApp, Title } from './App.styled';
+import {ContactForm} from './Form/Form';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+filterContacts = event => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+onFilterChange = event => {
+    const { value } = event.target;
+    this.setState({ filter: value });
+  };
+
+ addContact = (name, number) => {
+  console.log(name)
+  if(this.state.contacts.find(contact => contact.name === name)){
+    return alert('${contact.name} is already in contacts')
+  };
+
+  this.setState(prevState => ({
+    contacts : [...prevState.contacts, {
+      id:nanoid(), 
+      name, 
+      number}]
+  }))
+}
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+
+    return (
+      <StyledApp>
+        <Title>Phonebook</Title>
+
+        <ContactForm onSubmit={this.addContact}></ContactForm>
+
+        <Title>Contacts</Title>
+
+        <Filter name={this.state.filter} onFilterChange={this.onFilterChange}></Filter>
+
+        <ContactList contacts={this.filterContacts()} onDelete={this.deleteContact}></ContactList>
+
+      </StyledApp>
+    );
+  }
+}
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const StyledApp = styled.div`
+  height: 100vh;
+  font-size: 40px;
+  color: #010101;
+  padding-top: 30px;
+  padding-left: 30px;
+`;
+
+export const Title = styled.h1`
+  font-size: 26px;
+    color: #516aed;
+`
